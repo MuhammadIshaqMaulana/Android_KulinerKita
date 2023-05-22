03bccfd905289c63a8e7ec6598d896707e12179a
Muhammad Ishaq Maulana <115702052+MuhammadIshaqMaulana@users.noreply.github.com>
2023-05-22 09:29:40 +0700

    menyelesaikan adapter kuliner

--- app/src/main/java/com/ishaq/kulinerkita/Activity/TambahActivity.java
+++ app/src/main/java/com/ishaq/kulinerkita/Activity/TambahActivity.java
@@ -1,5 +1,6 @@
 package com.ishaq.kulinerkita.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -70,6 +71,8 @@ public class TambahActivity extends AppCompatActivity {
                 String pesan = response.body().getPesan();
 
                 Toast.makeText(TambahActivity.this, "Kode: " + kode + "Pesan: " + pesan, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(TambahActivity.this, MainActivity.class);
+                finish();
             }
 
             @Override
